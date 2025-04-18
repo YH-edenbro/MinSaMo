ca69701d8fd86af520140b85092d908b8809625e
YH-edenbro <hyun96227@gmail.com>
2025-04-18 15:05:03 +0900

    acoounts all

--- 04-pjt-skeleton/mypjt/urls.py
+++ 04-pjt-skeleton/mypjt/urls.py
@@ -25,5 +25,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("books/", include("books.urls")),
+    path("accounts/", include("accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
